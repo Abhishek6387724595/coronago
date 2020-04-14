591028488399eeb2cd6fea4b2b6abb995ad95c72
Abhishek Awasthi <avasthiabhishek0033@gmail.com>
2020-04-15 00:50:52 +0530

    api for live updates

--- corona.php
+++ corona.php
@@ -1,7 +1,15 @@
 <?php
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);  
 
-$data = file_get_contents('https://api.covid19api.com/summary');
-$coronadata = json_decode($data);
-//echo "<pre>";
+$response = file_get_contents("https://api.covid19api.com/summary", false, stream_context_create($arrContextOptions));
+
+$coronadata = json_decode($response);
+echo "<pre>";
+//echo $response; 
 print_r($coronadata);
 ?>
